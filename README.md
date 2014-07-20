f6a42953296c78a207e518040416f74297638a2e
Goh'Dan <gohdan@gohdan.ru>
2014-07-20 15:29:40 +0400

    Initial state of the project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 sinch
 =====
 
-Site INtegrity CHecker
+sinch is Site INtegrity CHecker. It's the bash script that gets current state of the sites by FTP using lftp and compares it to master copy using diff. It's intended to run periodically by cron. Warning: never run it as a privileged user, it uses FTP mirroring and can wipe your filesystem out in case of error.
---
+++ sinch.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+ORIGIFS=$IFS
+ORIGOFS=$OFS;
+
+IFS=$(echo -en "\n\b")
+OFS=$(echo -en "\n\b")
+
+ROOT_PATH=$(cd $(dirname $0) && pwd);
+CONF_PATH=$ROOT_PATH/config
+SITE_PATH=$ROOT_PATH/sites
+
+
+for i in `ls $CONF_PATH`; do
+
+	if [ "example" != "$i" ]; then
+
+		echo "====== SITE: $i ======"
+		echo ''
+
+		source $CONF_PATH/$i
+
+		HOST_PATH=$SITE_PATH/$i
+
+		if [ -e $HOST_PATH ]; then
+
+			MASTER_PATH=$HOST_PATH/master
+		
+			if [ -e $MASTER_PATH ]; then
+
+				CURRENT_PATH=$HOST_PATH/current
+				if [ -e $CURRENT_PATH ]; then
+
+					# FTP block
+
+					FTPURL="ftp://$USER:$PASS@$HOST"
+
+					lftp -c "set ftp:list-options -a;
+					open '$FTPURL';
+					lcd '$ROOT_PATH';
+					lcd '$SITE_PATH';
+					lcd '$CURRENT_PATH';
+					cd '$RDIR';
+					mirror -e --verbose --exclude-glob $EXCL
+					bye"
+
+					# end: FTP block
+
+					diff -bqr $MASTER_PATH $CURRENT_PATH
+
+					echo ''
+
+				else
+					echo "ERROR: no directory "$CURRENT_PATH
+				fi
+
+			else
+				echo "ERROR: no directory $MASTER_PATH, create and put master copy there"
+			fi
+
+		else
+			echo "ERROR: no directory "$HOST_PATH
+		fi
+	fi
+
+done
+
+IFS=$ORIGIFS
+OFS=$ORIGOFS
+
